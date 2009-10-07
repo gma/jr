723becb70dae810c4c0d15e2f78a7f0a47858067
Jon Ramsey <jonathon.ramsey@gmail.com>
2009-10-07 18:15:46 +0100

    Adding basic database support for our tests.

--- test/app_test.rb
+++ test/app_test.rb
@@ -2,14 +2,33 @@ require File.join(File.dirname(__FILE__), *%w[.. app])
 require 'test/unit'
 require 'rack/test'
 require 'shoulda'
+require 'rake'
+require 'sinatra/activerecord/rake'
+require 'active_support/testing/assertions'
 
 set :environment, :test
 
 class SchedulerTest < Test::Unit::TestCase
   include Rack::Test::Methods
+  include ActiveSupport::Testing::Assertions
   
   def app
     Sinatra::Application
+  end
+
+  def setup
+    ENV["VERSION"] = "0"
+    Rake::Task["db:migrate"].invoke
+    ENV.delete("VERSION")
+    Rake::Task["db:migrate"].invoke
+  end
+
+  context "POST /jobs" do
+    should_eventually "create a new job with state running" do
+      assert_difference "Job.count" do
+        post "/jobs"
+      end
+    end
   end
 
   context "GET /jobs/123" do
