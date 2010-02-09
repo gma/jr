49aa1651da163a01804903fc063380e694eb4fa2
Glenn Gillen <glenn@racoon.lan>
2010-02-09 15:54:26 +0000

    Attempt to reconnect when sql server goes away.

--- app.rb
+++ app.rb
@@ -101,6 +101,9 @@ post "/jobs/?" do
       job = Job.process(params[:name], config, params[:arguments])
       status(202)  # accepted
       job.id.to_s
+    rescue Mysql::Error
+      ActiveRecord::Base.connection.reconnect!
+      retry
     rescue JobRunner::JobNotFoundError => exception
       log.error "Exception caught: #{exception}"
     end
